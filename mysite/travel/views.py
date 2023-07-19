8dd623cc3f1691e1502a52c6bbf3bef6c180331e
Jerry <jamesharold1738@gmail.com>
2023-07-18 23:30:30 -0400

    added for loops with jinja in html

--- mysite/travel/views.py
+++ mysite/travel/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 
 def index(request):
-    return render(request,'index.html')
+    dest1 = Destination()
+    dest1.name = 'JERRRYYY'
+    dest1.description = 'Its actually Iron Man'
+
+    dest2 = Destination()
+    dest2.name= 'Tony Stark'
+    dest2.description = 'I am not Ironman'
+
+    dests = [dest1, dest2]
+    return render(request,'index.html', {'dests': dests})
